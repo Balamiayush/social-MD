6eed894bf7425ebfdda0735af658828ce3c82f9a
Balamiayush <aryanbalami54@gmail.com>
2024-10-08 08:44:03 +0545

    part2

--- index.js
+++ index.js
@@ -15,13 +15,16 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.render("index");
 });
+
 app.get("/login", async (req, res) => {
   res.render("login");
 });
 
-app.get('/profile',isLoggedIn,(req,res)=>{
-  res.render('login',{user:req.user});  
-})
+// Protecting post route with isLoggedIn middleware
+app.get("/post", isLoggedIn, (req, res) => {
+  res.render("login", { user: req.user });
+});
+
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
   let user = await User.findOne({ email });
@@ -33,6 +36,9 @@ app.post("/register", async (req, res) => {
       return res.status(500).send("Error generating salt");
     }
     bcrypt.hash(password, salt, async (err, hash) => {
+      if (err) {
+        return res.status(500).send("Error hashing password");
+      }
       let user = await User.create({
         name,
         email,
@@ -43,9 +49,7 @@ app.post("/register", async (req, res) => {
       res.status(201).send("User registered successfully");
     });
   });
-  // res.status(201).send("User registered successfully");
 });
-
 
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -55,25 +59,44 @@ app.post("/login", async (req, res) => {
   }
   bcrypt.compare(password, user.password, (err, isMatch) => {
     if (isMatch) {
-      return res.status(400).send("you can login");
+      // Generate JWT token and store it in a cookie
+      let token = jwt.sign({ email: email, userid: user._id }, "ayush");
+      res.cookie("token", token);
+      return res.redirect("/profile");
     } else {
-      return res.render("login");
+      return res.status(401).send("Incorrect password");
     }
   });
 });
+
 app.get("/logout", (req, res) => {
   res.clearCookie("token");
   res.redirect("/login");
 });
+
 function isLoggedIn(req, res, next) {
-  if (req.cookies.token === "") return res.send("you must be logged in");
-  else {
-    let data = jwt.verify(req.cookies.token, "ayush");
-    req.User = data
+  const token = req.cookies.token;
+  if (!token) return res.send("You must be logged in");
+  try {
+    let data = jwt.verify(token, "ayush");
+    req.user = data;
+    next();
+  } catch (err) {
+    return res.send("Invalid token");
   }
-  next();
 }
 
+app.get("/profile", isLoggedIn, async (req, res) => {
+  let user = await User.findOne({ email: req.user.email });
+  res.render("profile", { user: user });
+  // Get user's posts
+  let {content} = req.body;
+  let post = await Post.find({ user: user._id,content });
+  user.posts.push(post._id);
+  await user.save();
+  res.render("profile", { user: user, posts: post });
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on port `);
+  console.log(`Server is running on port ${port}`);
 });
